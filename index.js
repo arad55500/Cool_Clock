7ab08f659dd350196a435d0eeec8b7ed24d994ca
Arad Okanin <arad55501@gmail.com>
2024-02-06 13:42:11 -0500

    big update

--- index.js
+++ index.js
@@ -1,19 +1,36 @@
 const { app, BrowserWindow } = require('electron');
+const path = require('path');
 
-function createWindow () {
+function createWindow() {
   const win = new BrowserWindow({
-    width: 500,
-    height: 200,
+    width: 700,
+    height: 700,
     resizable: false,
     autoHideMenuBar: true,
-    title: 'LOADING...',
-    alwaysOnTop: true,
+    title: 'Cool Clock',
     webPreferences: {
-      nodeIntegration: true
+      preload: path.join(__dirname, 'preload.js'),
+      contextIsolation: true,
+      nodeIntegration: false,
     }
   });
 
   win.loadFile('index.html');
+
+  // Uncomment the following line if you want to open dev tools by default:
+  // win.webContents.openDevTools();
 }
 
 app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow();
+  }
+});
