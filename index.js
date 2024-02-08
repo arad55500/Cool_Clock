b7293b5cc3976017d1620df58be2172913315345
Arad Okanin <arad55501@gmail.com>
2024-02-08 11:00:35 -0500

    add more comments

--- index.js
+++ index.js
@@ -15,20 +15,25 @@ function createWindow() {
     }
   });
 
+  // and load the index.html of the app.
   win.loadFile('index.html');
 
   // Uncomment the following line if you want to open dev tools by default:
   // win.webContents.openDevTools();
 }
 
+// This method will be called when Electron has finished
 app.whenReady().then(createWindow);
 
+// Quit when all windows are closed.
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit();
   }
 });
 
+
+// In this file you can include the rest of your app's specific main process
 app.on('activate', () => {
   if (BrowserWindow.getAllWindows().length === 0) {
     createWindow();
