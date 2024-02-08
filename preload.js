fbd5c1730daef1e9126f609bc724ba530483db40
Arad Okanin <arad55501@gmail.com>
2024-02-08 11:01:22 -0500

    update preload.js

--- preload.js
+++ preload.js
@@ -1,5 +1,6 @@
 const { contextBridge } = require('electron');
 
+// Expose protected methods that allow the renderer process to use
 contextBridge.exposeInMainWorld('electronAPI', {
   setLocalStorage: (key, value) => localStorage.setItem(key, value),
   getLocalStorage: (key) => localStorage.getItem(key)
